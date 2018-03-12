757e6dd3e308542264471edf4957648ac0edcbfd
Leebsun <gkgktmdfl@naver.com>
2018-03-12 10:36:14 +0900

    이상없음

--- src/main/java/com/kid/member/MemberDAO.java
+++ src/main/java/com/kid/member/MemberDAO.java
@@ -1,5 +1,13 @@
 package com.kid.member;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDAO {
+	
+	private final String NAMESPACE = "memberMapper.";
 
 }
