4ec5e1d5a735727f7bb43260ab77bd66c03df10b
Leebsun <gkgktmdfl@naver.com>
2018-03-12 10:57:34 +0900

    일단 해결

--- src/main/java/com/kid/ctrl/MemberController.java
+++ src/main/java/com/kid/ctrl/MemberController.java
@@ -1,16 +1,134 @@
 package com.kid.ctrl;
 
+import java.lang.ProcessBuilder.Redirect;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
+import javax.inject.Inject;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.kid.member.MemberDTO;
+import com.kid.member.MemberService;
+
 
 @Controller
 @RequestMapping(value="/member/*")
 public class MemberController {
 	
-	@RequestMapping(value="memberJoin", method=RequestMethod.GET)
-	public String memberJoin(){
-		return "member/memberJoin";
+	@Inject
+	private MemberService memberService;
+	
+
+//일반회원 아이디 체크
+	@RequestMapping(value="memberIdCheck")
+	public ModelAndView memberIdCheck(String checkId, ModelAndView mv) throws Exception {
+		String result = "";
+		if(checkId.equals(""))
+		{
+			result = "아이디를 입력해주세요.";
+		}
+		else
+		{
+			result = memberService.memberIdCheck(checkId);			
+		}
+		
+		mv.addObject("result", result);
+		mv.setViewName("common/ajax");
+		
+		return mv;
 	}
+	
+//일반회원 회원가입 GET
+	@RequestMapping(value="memberJoin", method=RequestMethod.GET)
+	public ModelAndView memberJoin(MemberDTO memberDTO, ModelAndView mv, HttpSession session) throws Exception {
+
+		if(session.getAttribute("naverId") != null)
+		{
+			session.removeAttribute("naverId");
+		}
+		if(session.getAttribute("naverName") != null)
+		{
+			session.removeAttribute("naverName");
+		}
+		mv.addObject("menuTitle", "회원가입");
+		mv.setViewName("member/memberJoin");
+		
+		return mv;
+	}
+//일반회원 회원가입 POST
+	@RequestMapping(value="memberJoin", method=RequestMethod.POST)
+	public String memberJoin(MemberDTO memberDTO, RedirectAttributes rd, HttpSession session) throws Exception {
+		
+		rd = memberService.memberJoin(memberDTO, rd, session);	
+		
+		return "redirect:/";
+	}
+//일반회원 로그인	
+	@RequestMapping(value="memberLogin")
+	public String memberLogin(String logId, String logPw, HttpSession session, RedirectAttributes rd) {
+		String message = "로그인 실패";
+		MemberDTO memberDTO = new MemberDTO();
+
+		
+		memberDTO.setId(logId);
+		memberDTO.setPw(logPw);
+		
+		try {
+			memberDTO = memberService.memberLogin(memberDTO);
+	
+		} catch(Exception e) {
+			memberDTO = null;
+		
+			e.printStackTrace();
+		}
+		
+		if(memberDTO != null)
+		{
+			message = "로그인 성공";
+			
+			session.setAttribute("member", memberDTO);
+		}
+		
+		rd.addFlashAttribute("message", message);
+		
+		return "redirect:/";
+	}
+	
+//로그아웃
+	@RequestMapping(value="memberLogout")
+	public String logout(RedirectAttributes rd, HttpSession session) throws Exception {
+		
+		int result = memberService.stateUpdate((MemberDTO)session.getAttribute("member"));
+		if(result > 0)
+		{
+			rd.addFlashAttribute("message", "로그아웃 되었습니다.");
+			session.invalidate();
+		}
+		else
+		{
+			rd.addFlashAttribute("message", "로그아웃 실패");
+		}
+		
+		return "redirect:/";
+	}
+	
 
 }
+
